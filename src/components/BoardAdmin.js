c25ecd56bc329ffe90cd18fd694e987b673d4f20
MattiBSH <cph-mh829@cphbusiness.dk>
2022-05-13 15:02:56 +0200

    delete and get all users

--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -2,6 +2,28 @@ import React, { useState, useEffect } from "react";
 import UserService from "../services/user.service";
 const BoardAdmin = () => {
   const [content, setContent] = useState("");
+  const[users,setUsers]=useState([])
+
+
+  function deleteUser(email) {
+    UserService.deleteUser(email);
+    UserService.getAllUsers().then(
+      (response) => {
+        setUsers(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setContent(_content);
+      }
+    );
+
+  }
+
   useEffect(() => {
     UserService.getAdminBoard().then(
       (response) => {
@@ -17,12 +39,44 @@ const BoardAdmin = () => {
         setContent(_content);
       }
     );
-  }, []);
+    UserService.getAllUsers().then(
+      (response) => {
+        setUsers(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setContent(_content);
+      }
+    );
+  }, [users]);
+
   return (
     <div className="container">
       <header className="jumbotron">
         <h3>{content}</h3>
+        
       </header>
+      <table className="table">
+        <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                    </tr>
+                </thead>
+        {users!=null&&users.length!=null?users.map((user)=> {return <tr>
+                    <td>{user.id}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <button onClick={()=>deleteUser(user.email)} className="btn-danger btn-block btn-sm">Delete</button>
+                </tr>}):""}
+        </table>
     </div>
   );
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 const API_URL = "http://localhost:8080/api/test/";
+const API_URL_USER = "http://localhost:8080/api/user/";
+
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
 };
@@ -13,10 +15,18 @@ const getModeratorBoard = () => {
 const getAdminBoard = () => {
   return axios.get(API_URL + "admin", { headers: authHeader() });
 };
+const getAllUsers = () => {
+  return axios.get(API_URL_USER + "all", { headers: authHeader() });
+};
+const deleteUser = (email) => {
+  return axios.delete(API_URL_USER + "delete/", { headers: authHeader(),params: { email:email }});
+};
 const UserService = {
   getPublicContent,
   getUserBoard,
   getModeratorBoard,
   getAdminBoard,
+  getAllUsers,
+  deleteUser
 };
 export default UserService;
